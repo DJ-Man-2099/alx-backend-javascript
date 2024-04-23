b8724e71531836d6b9d9b7c8d4dada52b8c9a0c7
DJ-Man-2099 <davidmesak@hotmail.com>
2024-04-23 23:29:39 +0200

    Task 7

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,20 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default (firstName, lastName, fileName) => Promise
-  .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then((values) => values.map((response) => ({
-    status: response.status,
-    value: response.status === 'fulfilled' ? response.value : String(response.reason),
-  })));
+export default async (firstName, lastName, fileName) => {
+  const user = await signUpUser(firstName, lastName).then((user) => ({
+    value: user,
+    status: 'fulfilled',
+  })).catch((error) => ({
+    value: String(error),
+    status: 'rejected',
+  }));
+  const photo = await uploadPhoto(fileName).then((user) => ({
+    value: user,
+    status: 'fulfilled',
+  })).catch((error) => ({
+    value: String(error),
+    status: 'rejected',
+  }));
+  return [user, photo];
+};
