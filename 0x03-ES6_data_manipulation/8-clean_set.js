3cdbb2eedf04840173de96eeb381794b31e52c76
DJ-Man-2099 <davidmesak@hotmail.com>
2024-04-29 23:22:19 +0300

    Task 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,4 @@
 export default (set, startString) => {
-  if (!startString) {
-    return '';
-  }
   const stringLength = startString.length;
   const result = [...set.values()].filter((value) => value.startsWith(startString))
     .map((value) => value.slice(stringLength));
