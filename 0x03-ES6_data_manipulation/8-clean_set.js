3663c1ecbe7de6b9fb2181310e45255333345b1b
DJ-Man-2099 <davidmesak@hotmail.com>
2024-04-29 23:21:52 +0300

    Task 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default (set, startString) => {
-	if (!startString) {
-		return '';
-	}
-	const stringLength = startString.length;
-	const result = [...set.values()].filter((value) => value.startsWith(startString))
-		.map((value) => value.slice(stringLength));
-	return result.join('-');
+  if (!startString) {
+    return '';
+  }
+  const stringLength = startString.length;
+  const result = [...set.values()].filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(stringLength));
+  return result.join('-');
 };
