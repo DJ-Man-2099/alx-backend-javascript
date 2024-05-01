3ec4e3300b3accbc9eb501f78cf07b1cc62244bc
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-02 01:43:44 +0300

    Task 5

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,62 +1,36 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+
 const testing = document.createElement('div');
 testing.textContent = 'Testing';
 
 document.body.appendChild(testing);
-interface Teacher {
-	readonly firstName: string;
-	readonly lastName: string;
-	fullTimeEmployee: boolean;
-	yearsOfExperience?: number;
-	location: string;
-	[attributeName: string]: any;
+
+namespace Subjects {
+	export const cpp = new Cpp();
+	export const java = new Java();
+	export const react = new React();
+	export const cTeacher: Teacher = {
+		firstName: 'David',
+		lastName: 'John',
+		experienceTeachingC: 10
+	};
+
+	console.log('C++');
+	cpp.setTeacher(cTeacher);
+	console.log(cpp.getRequirements());
+	console.log(cpp.getAvailableTeacher());
+
+	console.log('Java');
+	java.setTeacher(cTeacher);
+	console.log(java.getRequirements());
+	console.log(java.getAvailableTeacher());
+
+	console.log('React');
+	react.setTeacher(cTeacher);
+	console.log(react.getRequirements());
+	console.log(react.getAvailableTeacher());
 }
-
-interface Directors extends Teacher {
-	numberOfReports: number;
-}
-
-const teacher3: Teacher = {
-	firstName: 'John',
-	fullTimeEmployee: false,
-	lastName: 'Doe',
-	location: 'London',
-	contract: false
-};
-
-console.log(teacher3);
-
-const director1: Directors = {
-	firstName: 'John',
-	lastName: 'Doe',
-	location: 'London',
-	fullTimeEmployee: true,
-	numberOfReports: 17
-};
-console.log(director1);
-
-interface printTeacherFunction {
-	(firstName: string, lastName: string): string;
-}
-
-const printTeacher: printTeacherFunction = (
-	firstName: string,
-	lastName: string
-) => `${firstName.charAt(0)}. ${lastName}`;
-
-console.log(printTeacher('John', 'Doe'));
-
-class StudentClass {
-	firstName: string;
-	lastName: string;
-	constructor(firstName: string, lastName: string) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-	workOnHomework = () => 'Currently working';
-	displayName = () => this.firstName;
-}
-
-const class1 = new StudentClass('John', 'Doe');
-
-console.log(class1.workOnHomework());
-console.log(class1.displayName());
