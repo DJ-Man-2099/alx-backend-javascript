dc640946afe3b7503f0ea90bb7496d6dd7f8b78f
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-18 20:56:41 +0300

    Task 2

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+// Create an interface for input and output
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Welcome to Holberton School, what is your name?");
+
+// Prompt the user for their name
+rl.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+
+  // Don't forget to close the readline interface
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
+  rl.close();
+  process.exit(0);
+});
