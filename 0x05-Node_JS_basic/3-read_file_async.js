61be2173f92dd21257b3ec66ac6a652900e9c4a1
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-18 22:09:56 +0300

    ????

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,42 +1,46 @@
 const { readFile } = require('fs');
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+const countStudents = (path) => {
   return new Promise((resolve, reject) => {
-    readFile(fileName, (error, data) => {
-      if (error) {
-        reject(Error('Cannot load the database'));
-      } else {
-        const lines = data.toString().split('\n');
-        for (let i = 0; i < lines.length; i += 1) {
-          if (lines[i]) {
-            length += 1;
-            const field = lines[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-              students[field[3]].push(field[0]);
-            } else {
-              students[field[3]] = [field[0]];
-            }
-            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-              fields[field[3]] += 1;
-            } else {
-              fields[field[3]] = 1;
-            }
-          }
-        }
-        const l = length - 1;
-        console.log(`Number of students: ${l}`);
-        for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {
-            console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
-          }
-        }
-        resolve(data);
+    readFile(path, { encoding: 'utf8' }, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
+
+      const lines = data.toString().trim().split('\n');
+      // Process each line
+      const fields = {};
+      let count = 0;
+      const firstNameIndex = 0;
+      const fieldIndex = 3;
+      lines.forEach((line, index) => {
+        // Skip the header row
+        if (index === 0 || line.trim() === '') return;
+
+        count += 1;
+        // Split each line into columns. Assuming comma-separated values
+        const columns = line.split(',');
+        // Example: Log the first column (e.g., name)
+        const field = columns[fieldIndex];
+        const firstName = columns[firstNameIndex];
+        if (!(field in fields)) {
+          fields[field] = [firstName];
+        } else {
+          fields[field].push(firstName);
+        }
+      });
+      console.log(`Number of students: ${count}`);
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const size = fields[key].length;
+          const list = fields[key].join(', ');
+          console.log(`Number of students in ${key}: ${size}. List: ${list}`);
+        }
+      }
+      resolve(data);
     });
   });
-}
+};
 
 module.exports = countStudents;
