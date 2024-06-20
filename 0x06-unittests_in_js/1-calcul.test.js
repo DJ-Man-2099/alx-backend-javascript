b3f59a3aab3afe13a36654121cfe3ee351ae02e8
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-20 19:57:13 +0300

    Task 2

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -5,10 +5,12 @@ describe('Calculate Number', () => {
     assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
     assert.strictEqual(calculateNumber('SUM', 1.2, 3), 4);
     assert.strictEqual(calculateNumber('SUM', 1.7, 3), 5);
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     assert.strictEqual(calculateNumber('SUM', 1.49999999999, 3.399999999), 4);
   });
   it('SUBTRACT with edge cases', () => {
     assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), 2);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3), 2);
     assert.strictEqual(calculateNumber('SUBTRACT', 1.7, 3), 1);
     assert.strictEqual(calculateNumber('SUBTRACT', 1.49999999999, 3.399999999), 2);
@@ -16,8 +18,10 @@ describe('Calculate Number', () => {
   it('DIVIDE with edge cases', () => {
     assert.strictEqual(calculateNumber('DIVIDE', 3, 1), 3);
     assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3), 0.333333333333333333);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     assert.strictEqual(calculateNumber('DIVIDE', 3, 1.7), 1.5);
     assert.strictEqual(calculateNumber('DIVIDE', 3.599999999, 1.49999999999), 4);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     assert.strictEqual(calculateNumber('DIVIDE', 3.599999999, 0.39999999999), 'Error');
   });
 });
