ba83cb3a1243f34e1c36fa0cce174e837ff6fde1
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-20 19:28:06 +0300

    Task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,15 +2,18 @@ var assert = require('assert');
 var calculateNumber = require("./0-calcul.js");
 describe('Calculate Number', () => {
   it('should return 4', () => {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return 2', () => {
+    assert.strictEqual(calculateNumber(-1.2, 3), 2);
   });
   it('should return 5', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
   it('should return 5', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
   it('should return 6', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
